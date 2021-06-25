0d80e90187eae0cc8dc545c779262a77463a3cb6
Michael Savin <m.savin@corp.mail.ru>
2021-06-25 12:18:52 +0300

    Update path to config
    Signed-off-by: Michael Savin <m.savin@corp.mail.ru>

--- pyalimony/config.py
+++ pyalimony/config.py
@@ -2,25 +2,22 @@
 """
     Базовые параметры
 """
+import os
+
 from ruamel.yaml import YAML, YAMLError
-from pprint import pprint
 
 yaml = YAML(typ='safe', pure=True)
 
 CHILDRENS = []
 
-with open("config.yaml", 'r') as stream:
+CONFIG_FILE = os.getenv("HOME") + "/.config/alimony.yaml"
+
+with open(CONFIG_FILE, 'r') as stream:
     try:
         data = yaml.load(stream)
         CHILDRENS.extend(data['childrens'])
         SALARY = data['SALARY']
         # Рразмер удержаний по всем ИП
         PERCENT = data['PERCENT']
-        # pprint(CHILDRENS)
     except YAMLError as exc:
         print(exc)
-
-# Размер зарплаты после вычета налогов
-# SALARY = 230550.0
-# Рразмер удержаний по всем ИП
-# PERCENT = 70.0
